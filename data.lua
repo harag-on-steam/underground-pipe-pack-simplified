8f2f771b73de16ecefe9eb642b945dda23bdf7f9
Harag <harag@cortexx.net>
2024-11-14 18:42:44 +0100

    update to 2.0 and add option to only use pipes

--- data.lua
+++ data.lua
@@ -9,6 +9,8 @@ local variants = {
     "underground-cross",
 }
 
+local only_pipes = settings.startup["lafh_only_pipes"].value
+
 local tiers = {
     {
         coupler = "small-pipe-coupler",
@@ -46,72 +48,104 @@ local tiers = {
 
 for _, tier_data in pairs(tiers) do
     for _, variant in pairs(variants) do
-        local recipe = data.raw.recipe[variant .. tier_data.suffix .. "-pipe" ]
+        local recipe = data.raw.recipe[variant .. tier_data.suffix .. "-pipe"]
         if recipe then
             local swivels = 0
             local pipes = 0
             local couplers = 0
 
             for _, ingredient in ipairs(recipe.ingredients) do
-                if ingredient[1] == tier_data.pipe then
-                    pipes = pipes + ingredient[2]
-                elseif ingredient[1] == tier_data.segment then
-                    pipes = pipes + (ingredient[2] * tier_data.multiplier)
-                elseif ingredient[1] == tier_data.coupler then
-                    couplers = ingredient[2]
-                elseif ingredient[1] == "swivel-joint" then
-                    swivels = swivels + ingredient[2]
+                if ingredient.name == tier_data.pipe then
+                    pipes = pipes + ingredient.amount
+                elseif ingredient.name == tier_data.segment then
+                    pipes = pipes + (ingredient.amount * tier_data.multiplier)
+                elseif ingredient.name == tier_data.coupler then
+                    couplers = ingredient.amount
+                elseif ingredient.name == "swivel-joint" then
+                    swivels = swivels + ingredient.amount
                 end
             end
 
             local ingredients = {}
-            if swivels  > 0 then table.insert(ingredients, { "swivel-joint",        swivels  }) end
-            if couplers > 0 then table.insert(ingredients, { tier_data.coupler_new, couplers }) end
-            if pipes    > 0 then table.insert(ingredients, { tier_data.pipe,        pipes    }) end
+            if only_pipes then
+                if pipes    > 0 then table.insert(ingredients, {type="item", name=tier_data.pipe,        amount=pipes + swivels * 2 + couplers }) end
+            else
+                if swivels  > 0 then table.insert(ingredients, {type="item", name="swivel-joint",        amount=swivels  }) end
+                if couplers > 0 then table.insert(ingredients, {type="item", name=tier_data.coupler_new, amount=couplers }) end
+                if pipes    > 0 then table.insert(ingredients, {type="item", name=tier_data.pipe,        amount=pipes    }) end
+            end
 
             recipe.ingredients = ingredients
         end
     end
 end
 
-local function replace_ingredients(recipe_name, ingredients)
+local function replace_ingredients(recipe_name, ingredients, ingredients2)
     local recipe = data.raw.recipe[recipe_name]
-    if recipe then
-        recipe.ingredients = ingredients
+    if not recipe then return end
+
+    recipe.ingredients = ingredients
+    if not only_pipes and ingredients2 then
+        for _, ingredient in pairs(ingredients2) do
+            table.insert(recipe.ingredients, ingredient)
+        end
     end
 end
 
-replace_ingredients("underground-mini-pump", {
-    {"engine-unit", 1},
-    {"steel-plate", 1},
-    {"small-pipe-coupler", 2},
-    {"pipe", 10},
-})
+if mods["aai-industry"] then
+    replace_ingredients("underground-mini-pump", {
+        {type="item", name="electric-motor",     amount= 2},
+        {type="item", name="steel-plate",        amount= 1},
+        {type="item", name="pipe",               amount=12},
+    }, {
+        {type="item", name="small-pipe-coupler", amount= 2},
+    })
+else
+    replace_ingredients("underground-mini-pump", {
+        {type="item", name="engine-unit",        amount= 1},
+        {type="item", name="steel-plate",        amount= 1},
+        {type="item", name="pipe",               amount=12},
+    }, {
+        {type="item", name="small-pipe-coupler", amount= 2},
+    })
+end
 
 replace_ingredients("4-to-4-pipe", {
-    {"small-pipe-coupler", 4},
-    {"pipe", 21},
+    {type="item", name="pipe",               amount=25},
+}, {
+    {type="item", name="small-pipe-coupler", amount= 4},
 })
 
 replace_ingredients("underground-space-pump", {
-    {"engine-unit", 1},
-    {"se-heavy-girder", 1},
-    {"space-pipe-coupler", 2},
-    {"se-space-pipe", 10},
+    {type="item", name="electric-motor",     amount= 2},
+    {type="item", name="se-space-pipe",      amount=12},
+}, {
+    {type="item", name="se-heavy-girder",    amount= 1},
+    {type="item", name="space-pipe-coupler", amount= 2},
 })
 
-local hide = {
+local function hide_recipes(recipe_names)
+    for _, name in pairs(recipe_names) do
+        recipe = data.raw.recipe[name]
+        if recipe then
+            recipe.hidden = true
+        end
+    end
+end
+
+hide_recipes({
     "medium-pipe-coupler",
     "large-pipe-coupler",
     "underground-pipe-segment-t1",
     "underground-pipe-segment-t2",
     "underground-pipe-segment-t3",
     "underground-pipe-segment-space",
-}
+})
 
-for _, name in pairs(hide) do
-    recipe = data.raw.recipe[name]
-    if recipe then
-        recipe.hidden = true
-    end
+if only_pipes then
+    hide_recipes({
+        "swivel-joint",
+        "small-pipe-coupler",
+        "space-pipe-coupler",
+    })
 end
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "lafh_only_pipes",
+        setting_type = "startup",
+        default_value = true,
+        order = "a1",
+    },
+})
